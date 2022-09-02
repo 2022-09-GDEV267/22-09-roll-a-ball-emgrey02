bd4aeec9ef1ed0c5cc5521493182a56e04d81267
Emma Grey <emgrey02@gmail.com>
2022-09-02 09:38:45 -0400

    Allow player to move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  	public float speed = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  	private Rigidbody rb;
+  	private float movementX;
+  	private float movementY;
+
+  	// Start is called before the first frame update
+  	void Start()
+  	{
+		rb = GetComponent<Rigidbody>();
+  	}
+
+  	void OnMove(InputValue movementValue)
+  	{
+		Vector2 movementVector = movementValue.Get<Vector2>();
+
+    	movementX = movementVector.x;
+    	movementY = movementVector.y;
+  	}
+
+  	void FixedUpdate()
+  	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+		rb.AddForce(movement * speed);
+  	}
 }
